8d0b57afbf9b1701743ae9778af19a2ba72b8a75
ERE <ernesto_711@live.com.mx>
2022-02-02 21:26:42 -0600

    avance con los estilos de la pagina principal

--- test.js
+++ test.js
@@ -1,10 +1,15 @@
 
 //este codigo sirve para capturar la tecla presionada y su codigo
 
-document.addEventListener('keydown', (event) => {
-    var keyValue = event.key;
-    var codeValue = event.code;
+// document.addEventListener('keydown', (event) => {
+//     var keyValue = event.key;
+//     var codeValue = event.code;
   
-    console.log("keyValue: " + keyValue);
-    console.log("codeValue: " + codeValue);
-  }, false);
+//     console.log("keyValue: " + keyValue);
+//     console.log("codeValue: " + codeValue);
+//   }, false);
+
+
+
+
+
