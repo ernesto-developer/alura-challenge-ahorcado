21e5df1fe01e0b7aba323e5988dbc45ddf0f8f07
ERE <ernesto_711@live.com.mx>
2022-02-03 22:17:13 -0600

    se termino la navegaicon entre paginas

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var correctWords = [];
 var wrongWords = [];
 var firstTime = true;
 var completeboy = [];
+// ######################################## PARTE GRAFICA #################
+var btnIniciarJuego = document.querySelector(".btn_Iniciar_Juego");
+var btnAgregarNuevaPalabra = document.querySelector(".btn_agregar_palabra");
+var btnIngresarPalabra = document.querySelector(".text-ingrese");
+var campoDeTexto = document.querySelector(".campo-text-ingrese");
+var btnGuardarYEmpezar = document.querySelector(".btn_guardar_empezar");
+var canvasOn = document.querySelector(".canvas");
 
 
 
@@ -243,7 +250,8 @@ function drawGameBoard(){
       
       
 function keyEvent(){
-            
+   
+
         
         document.addEventListener("keydown", (event) => {
         
@@ -260,31 +268,82 @@ function keyEvent(){
            drawPoste();
            completeboy.forEach( fn => { fn(); });
            
+    
+    
+    } ) ;
 
-
-} ) ;
+    
             
         }
 
-        
-sortWord();
+// #####################################################################################
+// #####################################################################################
 
-if(firstTime){
-    drawGameBoard();
-    keyEvent();
-    posX = pantalla.width / 2 - 95;
-    posXWrongWords = pantalla.width / 2 -45;            
-    gridBoard();
-    drawPoste();
-    
-   
+function habilitarTableroDeJuego(){
 
-}else{
-    keyEvent();
+    document.querySelector(".btns_Pagina_Principal").classList.add("btns_Pagina_Principal-off");
+    document.querySelector(".btns_pantalla_del_juego").classList.add("btns_pantalla_del_juego-on");
+    document.querySelector(".canvas").classList.add("canvas-on");
+
+    document.querySelector(".btns_de_ingresar_palabra").classList.remove("btns_de_ingresar_palabra-on");
+    document.querySelector(".maximo_de_caracteres").classList.remove("maximo_de_caracteres-on");
+    campoDeTexto.classList.remove("campo-text-ingrese-on");
+
+
+
+}
+
+function pantallaDePalabraNueva(){
+
+    document.querySelector(".btns_Pagina_Principal").classList.add("btns_Pagina_Principal-off");
+    document.querySelector(".btns_de_ingresar_palabra").classList.add("btns_de_ingresar_palabra-on");
+    document.querySelector(".maximo_de_caracteres").classList.add("maximo_de_caracteres-on");
+    document.querySelector(".text-ingrese").classList.add("text-ingrese-on");
+
+}
+
+function habilitarCampoDeTexto(){
+
+    campoDeTexto.classList.add("campo-text-ingrese-on");
+    campoDeTexto.focus();
+    btnIngresarPalabra.classList.remove("text-ingrese-on");
 }
 
 
 
 
+
+sortWord();
+
+
+
+
+    if(firstTime){
+        drawGameBoard();
+        keyEvent();
+        posX = pantalla.width / 2 - 95;
+        posXWrongWords = pantalla.width / 2 -45;            
+        // gridBoard();
+        drawPoste();
+        
+       
+    
+    }else{
+        keyEvent();
+    }
+
+
+    
+    
+    btnIniciarJuego.onclick = habilitarTableroDeJuego;
+    btnAgregarNuevaPalabra.onclick = pantallaDePalabraNueva;
+    btnIngresarPalabra.onclick =  habilitarCampoDeTexto;
+    btnGuardarYEmpezar.onclick = habilitarTableroDeJuego;
+    
+    
+    
+    
+    // TODO: HACER QUE CUANDO SE HAGA CLICK EN EL BOTON DE EMPEZAR Y GUARDAR SE EJECUTE LA FUNCION
+    // DE keyevent PARA CORREGIR EL PROBLEMA DE LAS LETRAS
 
 
