421bd35ce7af22c499de81fcc8ea1947f0ea1f34
ERE <ernesto_711@live.com.mx>
2022-02-05 21:55:43 -0600

    fin de lo mas pesado solo quedan detalles

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var pantalla = document.querySelector("canvas");
 var pincel = pantalla.getContext("2d");
 var posX = pantalla.width / 2 - 95;
-var posXWrongWords = pantalla.width / 2 -45;
+var posXWrongWords = pantalla.width / 2 - 45;
 var posY = pantalla.height;
 
 var numeroDeClicks = 0;
 var desistir = false;
 var firstTime = true;
+var juegoGanado = false;
+var juegoPerdido = false;
+const intentos = 6;
 
-var palabras = ["CABALLO","PERRO","GATO","LAGARTO","EDIFICIO","COCINA"];
+var palabras = ["CABALLO", "PERRO", "GATO", "LAGARTO", "EDIFICIO", "COCINA"];
 var palabraNueva = "";
 var palabra = "";
 var correctWords = [];
@@ -23,24 +26,34 @@ var btnGuardarYEmpezar = document.querySelector(".btn_guardar_empezar");
 var canvasOn = document.querySelector(".canvas");
 var btnCancel = document.querySelector(".btn_cancelar");
 var btnDesistir = document.querySelector(".btn_desistir");
+var btnNuevoJuego = document.querySelector(".btn_nuevo_juego");
 
 
-      
+
 
 // #####################################################################################
 // #####################################################################################
 
-function habilitarTableroDeJuego(){
-   
-    if(campoDeTexto.value != ""){
+function habilitarTableroDeJuego() {
+
+    palabra = "";
+    correctWords = [];
+    wrongWords = [];
+    completeboy = [];
+    desistir = false;
+    numeroDeClicks = 0;
+
+    if (campoDeTexto.value != "") {
         palabraNueva = campoDeTexto.value;
         palabra = palabraNueva.toUpperCase();
 
 
-    }else{
+    } else {
         sortWord();
     }
-    
+
+    console.log(palabra);
+
 
     document.querySelector(".btns_Pagina_Principal").classList.add("btns_Pagina_Principal-off");
     document.querySelector(".btns_pantalla_del_juego").classList.add("btns_pantalla_del_juego-on");
@@ -50,24 +63,18 @@ function habilitarTableroDeJuego(){
     document.querySelector(".maximo_de_caracteres").classList.remove("maximo_de_caracteres-on");
     campoDeTexto.classList.remove("campo-text-ingrese-on");
     document.querySelector(".text-ingrese").classList.remove("text-ingrese-on");
+
+
     
-    if(firstTime){
-      
+
         drawGameBoard();
         keyEvent();
-        posX = pantalla.width / 2 - 95;
-        posXWrongWords = pantalla.width / 2 -45;            
-        // gridBoard();
-        drawPoste();
-            
-    }else{
-       
-        keyEvent();
-    }
+
+    
 
 }
 
-function pantallaDePalabraNueva(){
+function pantallaDePalabraNueva() {
 
     document.querySelector(".btns_Pagina_Principal").classList.add("btns_Pagina_Principal-off");
     document.querySelector(".btns_de_ingresar_palabra").classList.add("btns_de_ingresar_palabra-on");
@@ -76,14 +83,14 @@ function pantallaDePalabraNueva(){
 
 }
 
-function habilitarCampoDeTexto(){
+function habilitarCampoDeTexto() {
 
     campoDeTexto.classList.add("campo-text-ingrese-on");
     campoDeTexto.focus();
     btnIngresarPalabra.classList.remove("text-ingrese-on");
 }
 
-function paginaPrincipal(){
+function paginaPrincipal() {
     document.querySelector(".btns_Pagina_Principal").classList.remove("btns_Pagina_Principal-off");
     document.querySelector(".btns_de_ingresar_palabra").classList.remove("btns_de_ingresar_palabra-on");
     document.querySelector(".maximo_de_caracteres").classList.remove("maximo_de_caracteres-on");
@@ -95,39 +102,42 @@ function paginaPrincipal(){
     document.querySelector(".canvas").classList.remove("canvas-on");
 
     campoDeTexto.value = "";
-    console.log(campoDeTexto.value);
+
 
 }
 
 
-btnDesistir.addEventListener("click", ()=>{
+btnDesistir.addEventListener("click", () => {
+    console.log(numeroDeClicks);
 
-    
-    if(numeroDeClicks == 0){
-                
-        desistir  = true;
+    if (numeroDeClicks == 0) {
+
+        desistir = true;
+        wrongWords = [];
         drawGameBoard();
-        
-    }else{
-        
+
+
+    } else {
+
+        wrongWords = [];
         paginaPrincipal();
     }
-    
+
     numeroDeClicks = 1;
 
 });
 
 
-    
-    
-    btnIniciarJuego.onclick = habilitarTableroDeJuego;
-    btnAgregarNuevaPalabra.onclick = pantallaDePalabraNueva;
-    btnIngresarPalabra.onclick =  habilitarCampoDeTexto;
-    btnGuardarYEmpezar.onclick = habilitarTableroDeJuego;
-    btnCancel.onclick = paginaPrincipal;
-   
-    
-    
-    
+
+
+btnIniciarJuego.onclick = habilitarTableroDeJuego;
+btnAgregarNuevaPalabra.onclick = pantallaDePalabraNueva;
+btnIngresarPalabra.onclick = habilitarCampoDeTexto;
+btnGuardarYEmpezar.onclick = habilitarTableroDeJuego;
+btnCancel.onclick = paginaPrincipal;
+btnNuevoJuego.onclick = paginaPrincipal;
+
+
+
 
 
--- tableroDeJuego.js
+++ tableroDeJuego.js
@@ -1,153 +1,229 @@
 
 
-function drawBoy(){
+function drawBoy() {
 
 
     switch (wrongWords.length) {
-           case 1:
-                completeboy.push(boyHead);
-        
-                break;
-            case 2:
-                completeboy.push(boybody);
-                break;
-            case 3:
-                completeboy.push(boyLeftArm);
-                break;
-            case 4:
-                completeboy.push(boyRightArm);
-                break;
-            case 5:
-                completeboy.push(boyLeftFoot);
-                break;
-            case 6:
-                completeboy.push(boyRightFoot);
-                break;
-    
+        case 1:
+            completeboy.push(boyHead);
+
+            break;
+        case 2:
+            completeboy.push(boybody);
+            break;
+        case 3:
+            completeboy.push(boyLeftArm);
+            break;
+        case 4:
+            completeboy.push(boyRightArm);
+            break;
+        case 5:
+            completeboy.push(boyLeftFoot);
+            break;
+        case 6:
+            completeboy.push(boyRightFoot);
+            break;
+
         default:
             break;
     }
 
 
-  
+
 
 }
 
-function sortWord(){
+function sortWord() {
 
-    var numeroAsar =  Math.floor(Math.random() * palabras.length);
+    var numeroAsar = Math.floor(Math.random() * palabras.length);
     palabra = palabras[numeroAsar];
 
 
 }
-function drawWord(x , i , mostrar){
-        
+function drawWord(x, i, mostrar) {
+
     pincel.beginPath();
-    mostrar == false ? correctWords.includes(palabra[i])? pincel.fillStyle = "#0A3871" :  pincel.fillStyle = "transparent": 
-    pincel.fillStyle = "#0A3871";
-    
+    mostrar == false ? correctWords.includes(palabra[i]) ? pincel.fillStyle = "#0A3871" : pincel.fillStyle = "transparent" :
+        pincel.fillStyle = "#0A3871";
+
+
+
     pincel.font = "60px serif";
-    pincel.fillText(palabra[i],x,posY -60);
+    pincel.fillText(palabra[i], x, posY - 60);
+
 
 
 }
 
-function drawWrongWord(){
+function drawWrongWord() {
 
+    for (var i = 0; i < wrongWords.length; i++) {
 
-    for (let i = 0; i < wrongWords.length; i++) {
-        
         posXWrongWords = posXWrongWords + 40;
 
         pincel.beginPath();
         pincel.fillStyle = "#495057";
         pincel.font = "20px bold";
-        pincel.fillText(wrongWords[i],posXWrongWords,posY-10);
+        pincel.fillText(wrongWords[i], posXWrongWords, posY - 10);
     }
 
 
 }
 
-function centradoDePalbra(array, textPos){
+function centradoDePalbra(array, textPos) {
 
     switch (array.length) {
-    
+
         case 1:
             textPos == 1 ? posX : posXWrongWords;
             break;
         case 2:
-            textPos == 1 ? posX : posXWrongWords-= 20;
+            textPos == 1 ? posX : posXWrongWords -= 20;
             break;
         case 3:
-             textPos == 1 ?  posX-=25: posXWrongWords-=45;
+            textPos == 1 ? posX -= 25 : posXWrongWords -= 45;
             break;
         case 4:
-           textPos == 1 ? posX-=50 : posXWrongWords-=65 ;
+            textPos == 1 ? posX -= 50 : posXWrongWords -= 65;
             break;
         case 5:
-           textPos == 1 ? posX-= 75 : posXWrongWords-= 80;
+            textPos == 1 ? posX -= 75 : posXWrongWords -= 80;
             break;
         case 6:
-           textPos == 1 ? posX-=100 : posXWrongWords-=100;
+            textPos == 1 ? posX -= 100 : posXWrongWords -= 100;
             break;
         case 7:
-           textPos == 1 ? posX-=125 : posXWrongWords-=125;
+            textPos == 1 ? posX -= 125 : posXWrongWords -= 125;
             break;
         case 8:
-           textPos == 1 ? posX-=150 : posXWrongWords-=150;
+            textPos == 1 ? posX -= 150 : posXWrongWords -= 150;
             break;
         default:
             break;
     }
-    
-    }
- 
- //####################################### creacion del tablero de juego #######################
- function drawGameBoard(){
 
+}
+
+function revicionDePalabra() {
+
+    for (let i = 0; i < palabra.length; i++) {
+
+        if (correctWords.includes(palabra[i])) {
+            juegoGanado = true;
+            numeroDeClicks = 1;
+        } else {
+
+            juegoGanado = false;
+            break;
+        }
+
+
+    }
+
+}
+
+function finDeIntentos(){
+    if(wrongWords.length == intentos){
+        juegoPerdido = true;
+        desistir =true;
+    }
+    
+}
+
+//####################################### creacion del tablero de juego #######################
+function drawGameBoard() {
+
+
+
+    pincel.clearRect(0, 0, pantalla.width, pantalla.height);
+    drawPoste();
     centradoDePalbra(palabra, 1);
     centradoDePalbra(wrongWords, 2);
     drawWrongWord();
+    for (var i = 0; i < palabra.length; i++) {
 
-    for(var i = 0; i < palabra.length; i++){
-        
-         
-             posX = posX+50;
-             drawLineas(posX);
-             drawWord(posX,i,desistir);
-             
-             
-             
-            }
+        posX = posX + 50;
+        drawLineas(posX);
+        drawWord(posX, i, desistir);
 
-   
-   
-}   
 
-      
-function keyEvent(){
-   
-    console.log(wrongWords);
-    
-    
-    document.addEventListener("keydown", (event) => {
-        
-        console.log(wrongWords);
-        
-                var keyValue = event.key.toUpperCase();
-                firstTime = false;
-                palabra.includes(keyValue)?correctWords.push(keyValue):wrongWords.push(keyValue);
-                
-          pincel.clearRect(0,0,pantalla.width,pantalla.height);
-           drawGameBoard();
-           posX = pantalla.width / 2 - 95;
-           posXWrongWords = pantalla.width / 2 -45;  
-          // gridBoard();
-          drawBoy();
-           drawPoste();
-           completeboy.forEach( fn => { fn(); });
-           
-         
-    
-    } ) ;
-        }
+    }
+
+
+    posX = pantalla.width / 2 - 95;
+    posXWrongWords = pantalla.width / 2 - 45;
+    if (juegoGanado) {
+        alert("Lo consegiste has ganado");
+        document.removeEventListener("keydown", eventosDelTecaldo);
+        juegoGanado = false;
+
+    }
+    if(juegoPerdido){
+        alert("Lo siento pero perdiste");
+        document.removeEventListener("keydown", eventosDelTecaldo);
+        juegoPerdido = false;
+    }
+}
+
+function eventosDelTecaldo(event) {
+
+    var letaRevisada = Text(event.key);
+    var keyValue = letaRevisada.toUpperCase();
+
+    firstTime = false;
+    palabra.includes(keyValue) ? correctWords.push(keyValue) : wrongWords.push(keyValue);
+    revicionDePalabra();
+    finDeIntentos();
+    pincel.clearRect(0, 0, pantalla.width, pantalla.height);
+    drawGameBoard();
+    if (juegoGanado) { desistir = true; }
+    posX = pantalla.width / 2 - 95;
+    posXWrongWords = pantalla.width / 2 - 45;
+    drawBoy();
+    drawPoste();
+    completeboy.forEach(fn => { fn(); });
+    console.log(correctWords);
+
+    console.log(juegoGanado);
+
+}
+
+function keyEvent() {
+
+    document.addEventListener("keydown", eventosDelTecaldo);
+
+
+}
+
+
+
+
+
+
+
+function TextInput(string) {//solo letras y numeros
+    var out = '';
+    //Se añaden las letras validas
+    var filtro = 'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ';//Caracteres validos
+
+    for (var i = 0; i < string.length; i++)
+        if (filtro.indexOf(string.charAt(i)) != -1)
+            out += string.charAt(i);
+
+    console.log(out);
+    return out;
+}
+function Text(string) {//solo letras
+    var out = '';
+    var regExp = /^[a-z\s]+$/;//Caracteres validos
+    var regExpMayus = /^[A-Z\s]+$/;
+
+    if (regExp.test(string) || regExpMayus.test(string)) {
+
+        out = string;
+        return out;
+    } else {
+        return out;
+    }
+
+}
